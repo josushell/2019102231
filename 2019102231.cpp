372917b84015cafef726dd6d26271eccb212611e
JO SUE YEON <yoy07030@khu.ac.kr>
2020-10-16 11:01:10 +0900

    Update 2019102231.cpp

--- 2019102231.cpp
+++ 2019102231.cpp
@@ -2,8 +2,8 @@
 using namespace std;
 
 int main() {
-	for (int i = 1; i <= 9; i++) {
-		cout << i<<"´Ü" << endl;
+	for (int i = 1; i <= 8; i++) {
+		cout << i<<"Â´Ãœ" << endl;
 		for (int j = 1; j <= 9; j++) {
 			cout << i << " X " << j << " = " << i * j << endl;
 		}
